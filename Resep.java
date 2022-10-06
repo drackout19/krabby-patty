8b6ec34b2e549670cfbbd60262aee03a7d081068
Amirul Mukminin <amirulmukminin0619@gmail.com>
2022-10-06 10:41:58 +0700

    Plankton berhasil mencuri resep

---
+++ Resep.java
@@ -0,0 +1,8 @@
+class Resep {
+    public static void main(String[] args) {
+        System.out.println("Resep Krabby Patty");
+        System.out.println("------------------");
+        System.out.println("1. Bawang");
+        System.out.println("2. Ketimun");
+    }
+}
