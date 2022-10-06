bba6f9789a606e0e2bdd0b74d6cfb91f1db891d6
drackout19 <amirulmukminin535@gmail.com>
2022-10-06 11:02:37 +0700

    menambah resep krabby patty

--- Resep.java
+++ Resep.java
@@ -4,5 +4,7 @@ class Resep {
         System.out.println("------------------");
         System.out.println("1. Bawang");
         System.out.println("2. Ketimun");
+        System.out.println("3. Mayonais");
+        System.out.println("4. Acar");
     }
 }
